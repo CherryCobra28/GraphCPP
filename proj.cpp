1ffdd19efa387a0ebfb309d940312223f2cd7d24
Thomas Draycott <thomasdraycott88@gmail.com>
2024-10-16 14:04:48 +0100

    created unique pointer helper functions
    These ensure resources are destroyed when they
    go out of scope

--- proj.cpp
+++ proj.cpp
@@ -1,7 +1,20 @@
 #include <igraph.h>
 #include <iostream>
+#include <memory>
 #include <random>
 #include <unordered_set>
+
+template <typename T> void destroy(T *t);
+void destroy(igraph_vector_int_t *v) { igraph_vector_int_destroy(v); }
+void destroy(igraph_t *g) { igraph_destroy(g); }
+template <typename T> struct Destroyer {
+  void operator()(T *t) { destroy(t); }
+};
+template <typename T>
+std::unique_ptr<T, Destroyer<T>> igraph_make_uni_ptr(T *t) {
+  return std::unique_ptr<T, Destroyer<T>>(t);
+}
+
 double r_number() {
   std::random_device red;
   std::default_random_engine gen(red());
@@ -19,24 +32,26 @@ public:
 
 void infect(InfectGraph *G, double p_i) {
   igraph_vector_int_t neigh;
-  igraph_vector_int_init(&neigh, 0);
+  auto neigh_p = igraph_make_uni_ptr(&neigh);
+  igraph_vector_int_init(neigh_p.get(), 0);
   for (auto node : G->infected) {
-    igraph_neighbors(G->graph, &neigh, node, IGRAPH_ALL);
-    igraph_integer_t n_size = igraph_vector_int_size(&neigh);
+    igraph_neighbors(G->graph, neigh_p.get(), node, IGRAPH_ALL);
+    igraph_integer_t n_size = igraph_vector_int_size(neigh_p.get());
     for (igraph_integer_t i = 0; i < n_size; i++) {
       double r = r_number();
       if (r < p_i) {
-        G->infected.insert(VECTOR(neigh)[i]);
+        G->infected.insert(VECTOR(*neigh_p)[i]);
       }
     }
   }
-  igraph_vector_int_destroy(&neigh);
+  // igraph_vector_int_destroy(&neigh);
 }
 
 int main() {
   igraph_t seed;
+  auto seed_p = igraph_make_uni_ptr(&seed);
   igraph_integer_t n{5};
-  igraph_full(&seed, n, NULL, NULL);
+  igraph_full(seed_p.get(), n, NULL, NULL);
   igraph_t g;
   igraph_barabasi_game(&g, 10, 1.0, 3, NULL, true, 0, false,
                        IGRAPH_BARABASI_PSUMTREE, &seed);
@@ -46,6 +61,5 @@ int main() {
   for (auto x : G.infected) {
     printf("%" IGRAPH_PRId " ", x);
   }
-  igraph_destroy(&seed);
   return 0;
 }
